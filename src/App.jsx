8a173cd6ce7bb5fc543c22ed1a74f68c72406ef0
Darlena Leixo <darlen_guitar@hotmail.com>
2025-05-02 13:30:18 -0300

    Corrige export default em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
-/* Será preenchido na próxima etapa com o código da calculadora */
+function App() {
+    return (
+      <div>
+        <h1>Calculadora Simples CLT</h1>
+      </div>
+    )
+  }
+  
+  export default App    /* Será preenchido na próxima etapa com o código da calculadora */
