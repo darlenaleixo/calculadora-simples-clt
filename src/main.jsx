8fc827cb0e9c18984b4c989ee40e0dc0b8bbb068
DARLEN ALEIXO <darlen_guitar@hotmail.com>
2025-05-06 22:43:36 -0300

    Personalizar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import './index.css'
-import { ThemeProvider } from './theme'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
+  <React.StrictMode>
     <App />
-  </ThemeProvider>
-)
+  </React.StrictMode>
+);
