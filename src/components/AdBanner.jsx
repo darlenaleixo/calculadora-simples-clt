2ce86cdb1d32bca34bfc5f44d7d80a132f1749c0
DARLEN ALEIXO <darlen_guitar@hotmail.com>
2025-05-13 22:42:36 -0300

    anuncios - App modificado

---
+++ src/components/AdBanner.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+function AdBanner() {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error("Adsense error:", e);
+    }
+  }, []);
+
+  return (
+    <div className="my-6 text-center">
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-8349819515281456"
+        data-ad-slot="1234567890"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </div>
+  );
+}
+
+export default AdBanner;
